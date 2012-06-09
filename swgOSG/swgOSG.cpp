99ca9ed0d1c4495106463195b9b43e4046a0b5d3
Eric Barr <eb@anhstudios.com>
2012-06-09 03:45:47 -0400

    Added a prompt if no parameters are passed in to request a tre dir and a file to load.

--- swgOSG/swgOSG.cpp
+++ swgOSG/swgOSG.cpp
@@ -24,7 +24,12 @@
 #include <memory>
 #include <map>
 #include <string>
+#include <tuple>
 
+#ifdef WIN32
+#pragma warning( push )
+#pragma warning( disable : 4250 )
+#endif
 #include <osg/Geode>
 #include <osg/Geometry>
 #include <osg/LOD>
@@ -45,8 +50,141 @@
 
 #include <osgDB/Registry>
 #include <osgDB/ReaderWriter>
+#ifdef WIN32
+#pragma warning( pop )
+#endif
 
 #include <swgRepository.hpp>
+
+std::tuple<std::string, std::vector<std::string>> ProcessInput(int argc, char *argv[]);
+osg::ref_ptr<osg::Node> buildTerrain();
+
+int main( int argc, char **argv )
+{
+    std::string tre_directory;
+    std::vector<std::string> files_to_load;
+
+    try 
+    {
+        std::tie(tre_directory, files_to_load) = ProcessInput(argc, argv);
+    }
+    catch (std::exception& /* exception */)
+    {
+        exit(0);
+    }
+
+    for(unsigned int i = 0; i < tre_directory.size(); ++i)
+    {
+        if( tre_directory[i] == '\\' )
+        {
+            tre_directory[i] = '/';
+        }
+    }
+
+    if( *(tre_directory.rbegin()) != '/' )
+    {
+        tre_directory.push_back('/');
+    }
+
+    swgRepository repo(tre_directory);
+
+    osg::ref_ptr<osg::MatrixTransform> rootNode( new osg::MatrixTransform );
+
+    rootNode->setMatrix(
+        osg::Matrix::rotate(
+        	  osg::DegreesToRadians( 90.0 ),
+        	  1.0, 0.0, 0.0 ));
+
+    try 
+    {
+        for (auto& filename : files_to_load)
+        {
+            rootNode->addChild(repo.loadFile(filename));
+        }
+    } catch (std::exception& /* exception */) {
+        exit(0);
+    }
+        
+    // construct the viewer.
+    osgViewer::Viewer viewer;
+
+    // add model to viewer.
+    viewer.setSceneData( rootNode.get() );
+
+    //viewer.get
+
+    viewer.setUpViewInWindow( 20, 20, 640, 480 );
+    //viewer.setUpViewAcrossAllScreens();
+
+    viewer.addEventHandler( new osgViewer::ScreenCaptureHandler );
+    viewer.addEventHandler( new osgViewer::LODScaleHandler );
+    viewer.addEventHandler( new osgViewer::StatsHandler );
+
+    osg::ref_ptr<osgGA::KeySwitchMatrixManipulator> keyswitchManipulator
+      = new osgGA::KeySwitchMatrixManipulator;
+    
+    osg::ref_ptr<osgGA::TrackballManipulator>
+      tb = new osgGA::TrackballManipulator();
+
+    keyswitchManipulator->addMatrixManipulator('1',
+	    				     "Trackball",
+	    				     tb.get()
+	    				     );
+    keyswitchManipulator->addMatrixManipulator( '2',
+	    				      "Flight",
+	    				      new osgGA::FlightManipulator()
+	    				      );
+    keyswitchManipulator->addMatrixManipulator( '3',
+	    				      "Drive",
+	    				      new osgGA::DriveManipulator()
+	    				      );
+    keyswitchManipulator->addMatrixManipulator( '4',
+	    				      "Terrain",
+	    				      new osgGA::TerrainManipulator()
+	    				      );
+
+    viewer.setCameraManipulator( keyswitchManipulator.get() );
+
+    return viewer.run();
+}
+    
+std::tuple<std::string, std::vector<std::string>> ProcessInput(int argc, char *argv[])
+{
+    std::string swg_live_file;
+    std::vector<std::string> load_files;
+
+    if (argc == 1)
+    {
+        std::cout << "Enter a directory containing .tre files" << std::endl;
+        std::cin >> swg_live_file;
+        //ValidateSwgLiveConfig(swg_live_file);
+
+        std::string output_path;
+        std::cout << "\nEnter the path to a file in the archive (ex. appearance/mesh/boba_fett_l0.mgn)" << std::endl;
+        std::cin >> output_path;
+
+        load_files.push_back(output_path);
+        //ValidateTargetOutputDirectory(output_path);
+    }
+    else if (argc > 2)
+    {
+        swg_live_file = std::string(argv[1]);
+        //ValidateSwgLiveConfig(swg_live_file);
+        
+        unsigned int numFiles = (argc - 2);
+        for( unsigned int i = 0; i < numFiles; ++i )
+        {
+            load_files.push_back(argv[2+i]);
+        }
+        //ValidateTargetOutputDirectory(output_path);
+    }
+    else
+    {        
+        throw std::runtime_error("Invalid number of parameters specified");
+    }
+
+    return std::make_tuple(swg_live_file, load_files);
+}
 
 osg::ref_ptr<osg::Node> buildTerrain()
 {
@@ -87,96 +225,4 @@ osg::ref_ptr<osg::Node> buildTerrain()
   geode->addDrawable( geometry );
 
   return geode;
-}
-
-int main( int argc, char **argv )
-{
-  std::cout << "argc: " << argc << std::endl;
-
-  if( 3 > argc )
-    {
-      std::cout << "Usage: " << argv[0]
-		<< " <directory containing .tre files> "
-		<< " <path/to/file/in/tre/archive> "
-		<< std::endl;
-      return 0;
-    }
-
-  std::string treDirectory( argv[1] );
-  for( unsigned int i = 0; i < treDirectory.size(); ++i )
-    {
-      if( treDirectory[i] == '\\' )
-        {
-          treDirectory[i] = '/';
-        }
-    }
-
-  if( *(treDirectory.rbegin()) != '/' )
-    {
-      treDirectory.push_back( '/' );
-    }
-
-  swgRepository repo( treDirectory );
-
-  osg::ref_ptr<osg::MatrixTransform> rootNode( new osg::MatrixTransform );
-
-  rootNode->setMatrix(
-		      osg::Matrix::rotate(
-					  osg::DegreesToRadians( 90.0 ),
-					  1.0, 0.0, 0.0 )
-		      );
-
-  try {
-
-  unsigned int numFiles = ( argc - 2 );
-  for( unsigned int i = 0; i < numFiles; ++i )
-    {
-      std::string filename( argv[2+i] );
-      rootNode->addChild( repo.loadFile( filename ) );
-    }
-  } catch (std::exception& exception) {
-	  exit(0);
-  }
-
-  // construct the viewer.
-  osgViewer::Viewer viewer;
-
-  // add model to viewer.
-  viewer.setSceneData( rootNode.get() );
-
-  //viewer.get
-
-  viewer.setUpViewInWindow( 20, 20, 640, 480 );
-  //viewer.setUpViewAcrossAllScreens();
-
-  viewer.addEventHandler( new osgViewer::ScreenCaptureHandler );
-  viewer.addEventHandler( new osgViewer::LODScaleHandler );
-  viewer.addEventHandler( new osgViewer::StatsHandler );
-
-  osg::ref_ptr<osgGA::KeySwitchMatrixManipulator> keyswitchManipulator
-    = new osgGA::KeySwitchMatrixManipulator;
-  
-  osg::ref_ptr<osgGA::TrackballManipulator>
-    tb = new osgGA::TrackballManipulator();
-
-  keyswitchManipulator->addMatrixManipulator('1',
-					     "Trackball",
-					     tb.get()
-					     );
-  keyswitchManipulator->addMatrixManipulator( '2',
-					      "Flight",
-					      new osgGA::FlightManipulator()
-					      );
-  keyswitchManipulator->addMatrixManipulator( '3',
-					      "Drive",
-					      new osgGA::DriveManipulator()
-					      );
-  keyswitchManipulator->addMatrixManipulator( '4',
-					      "Terrain",
-					      new osgGA::TerrainManipulator()
-					      );
-
-  viewer.setCameraManipulator( keyswitchManipulator.get() );
-
-  return viewer.run();
 }
